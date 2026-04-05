bd7565711950753dc5ee5554419e33d513948e54
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-05 16:01:33 -0700

    Update Longest Substring with At Most K Distinct Characters.py

--- Longest Substring with At Most K Distinct Characters.py
+++ Longest Substring with At Most K Distinct Characters.py
@@ -1,13 +1,18 @@
+# Find the longest substring of s with at most k distinct characters.
+# Return the length.
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
         # Use dictionary d to keep track of (character, location) pair,
         # where location is the rightmost location that the character appears at
         d = {}
+
+        # low stores the frontmost character
+        # ret stores the length to return
         low, ret = 0, 0
         for i, c in enumerate(s):
-            d[c] = i
+            d[c] = i  # always stores rightmost place character appears
             if len(d) > k:
-                low = min(d.values())
+                low = min(d.values())  # low stores the frontmost character
                 del d[s[low]]
                 low += 1
             ret = max(i - low + 1, ret)
