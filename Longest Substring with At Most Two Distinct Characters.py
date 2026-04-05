60bd98ae28575659c0e0ba261c4500e2c3c6c2af
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-05 16:05:09 -0700

    Update Longest Substring with At Most Two Distinct Characters.py

--- Longest Substring with At Most Two Distinct Characters.py
+++ Longest Substring with At Most Two Distinct Characters.py
@@ -1,11 +1,13 @@
+# Find the longest substring of s with at most k distinct characters.
+# Return the length.
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
         cnt = {}
-        ans = 0
-        l = 0
+        ans = 0  # answer
+        l = 0  # frontmost character
         for r, x in enumerate(s):
             cnt[x] = cnt.get(x,0) + 1
-            while len(cnt) > 2:
+            while len(cnt) > 2: # move l rightwards until the third one is gone
                 cnt[s[l]] -= 1
                 if cnt[s[l]] == 0: del cnt[s[l]]
                 l += 1
