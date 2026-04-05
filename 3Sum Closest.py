87d1801d2105d0954c1b307f540da10a6fd260ee
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-05 09:00:42 -0700

    Add comments for clarity in threeSumClosest method
    Added comments to clarify the purpose of variables and logic.

--- 3Sum Closest.py
+++ 3Sum Closest.py
@@ -1,14 +1,16 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        minsum = nums[0] + nums[1] + nums[2]
+        minsum = nums[0] + nums[1] + nums[2]  # smallest sum
 
-        sortnums = sorted(nums)
+        sortnums = sorted(nums)  # sorting lets you make assumptions about changes
 
-        for x in range(len(sortnums)):
+        for x in range(len(sortnums)):  # sortnums[x] is the smallest
             left = x + 1
             right = len(sortnums) - 1
             while left < right:
+                
                 newsum = sortnums[x] + sortnums[left] + sortnums[right]
+                
                 if newsum == target:
                     return newsum
                 elif abs(target - newsum) < abs(target - minsum):
