499d55034f54adab6c1712fd8987e180f145961c
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-06 00:53:55 -0700

    Update Square Matrix Multiplication.java

--- Square Matrix Multiplication.java
+++ Square Matrix Multiplication.java
@@ -1,13 +1,17 @@
+/*
+This implements square matrix multiplication.
+*/
 public class Kata {
 
 	public static int[][] matrixMultiplication(int[][] a, int[][] b) {
     int sideLength = a.length;
     int[][] answer = new int[sideLength][sideLength];
     
-    for(int ycoord = 0; ycoord < sideLength; ++ycoord) {
-      for(int xcoord = 0; xcoord < sideLength; ++xcoord) {
+    for(int ycoord = 0; ycoord < sideLength; ++ycoord) { // this is the y-coordinate of the first one
+      for(int xcoord = 0; xcoord < sideLength; ++xcoord) { // this is the x-coordinate of the second one
         int yx = 0;
         for(int shifter = 0; shifter < sideLength; ++shifter) {
+		  // matrix multiplication entails adding these numbers like this
           yx += a[ycoord][shifter] * b[shifter][xcoord];
         }
         answer[ycoord][xcoord] = yx;
